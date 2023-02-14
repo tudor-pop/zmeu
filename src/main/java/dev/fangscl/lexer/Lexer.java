c402b6638de87314207c90674616af6b607bf2f8
Tudor Pop <tudor14pop@gmail.com>
2023-02-15 00:15:36 +0200

    Pull algorithm into function

--- src/main/java/dev/fangscl/lexer/Lexer.java
+++ src/main/java/dev/fangscl/lexer/Lexer.java
@@ -21,57 +21,11 @@ public class Lexer {
         var iterator = new StringCharacterIterator(source);
         for (char i = iterator.first(); iterator.current() != CharacterIterator.DONE; i = iterator.next()) {
             if (Character.isDigit(i) || i == '.') {
-                /* parse the number if there are multiple digits */
-                var tokenString = new StringBuilder(2);
-
-                for (char j = i; j != CharacterIterator.DONE; j = iterator.next()) {
-                    /* j stops at the next character after the number eg 12;
-                     * i must be at position of number 2 because it will get incremented to ; at the end of the top loop
-                     * */
-                    if (Character.isDigit(j)) {
-                        tokenString.append(j);
-                    } else if (j == '.') {
-                        // valid input: 1.2 , .5 , 0.522123f
-                        for (char k = j; k != CharacterIterator.DONE; k = iterator.next()) {
-                            if (tokenString.length() > 0) {
-                                int j2 = tokenString.charAt(tokenString.length() - 1);
-                                if (j2 == 'f' || j2 == 'd') {
-                                    // .2dddd or 0.223ffff ... multiple f/d not allowed or just break
-                                    throw new LexerTokenException("Invalid decimal number");
-                                }
-                            }
-                            tokenString.append(k); // add the "." to the number
-                        }
-                        var token = new Token(tokenString.toString(), TokenType.Decimal);
-                        tokens.add(token);
-                        // invalid input: 2.  0.d
-                    } else {
-                        i = iterator.previous();
-                        break;
-                    }
-                }
-                var token = new Token(tokenString.toString(), TokenType.toNumber(i));
+                Token token = handleDigit(iterator, i);
                 tokens.add(token);
             } else if (Character.isAlphabetic(i)) {
-                /* parse the keyword if there are multiple digits */
-                var tokenString = new StringBuilder(3);
-                for (char j = i; j != CharacterIterator.DONE; j = iterator.next()) {
-                    if (Character.isAlphabetic(j)) {
-                        tokenString.append(j);
-                    } else {
-                        iterator.previous();
-                        break;
-                    }
-                }
-                String keyword = tokenString.toString();
-                TokenType type = TokenType.toKeyword(keyword);
-                if (type == TokenType.Unknown) {
-                    var token = new Token(keyword, TokenType.Identifier);
-                    tokens.add(token);
-                } else {
-                    var token = new Token(keyword, type);
-                    tokens.add(token);
-                }
+                var token = handleAlphabetic(iterator, i);
+                tokens.add(token);
             } else if (TokenType.isSymbol(i)) {
                 var token = new Token(i, TokenType.toSymbol(i));
                 tokens.add(token);
@@ -83,6 +37,60 @@ public class Lexer {
 
         }
         return tokens;
+    }
+
+    private Token handleAlphabetic(StringCharacterIterator iterator, char i) {
+        /* parse the keyword if there are multiple digits */
+        var tokenString = new StringBuilder(3);
+        for (char j = i; j != CharacterIterator.DONE; j = iterator.next()) {
+            if (Character.isAlphabetic(j)) {
+                tokenString.append(j);
+            } else {
+                iterator.previous();
+                break;
+            }
+        }
+        String keyword = tokenString.toString();
+        TokenType type = TokenType.toKeyword(keyword);
+        if (type == TokenType.Unknown) {
+            return new Token(keyword, TokenType.Identifier);
+        } else {
+            return new Token(keyword, type);
+        }
+    }
+
+    private Token handleDigit(StringCharacterIterator iterator, char i) {
+        /* parse the number if there are multiple digits */
+        var tokenString = new StringBuilder(2);
+
+        for (char j = i; j != CharacterIterator.DONE; j = iterator.next()) {
+            /* j stops at the next character after the number eg 12;
+             * i must be at position of number 2 because it will get incremented to ; at the end of the top loop
+             * */
+            if (Character.isDigit(j)) {
+                tokenString.append(j);
+            } else if (j == '.') {
+                // valid input: 1.2 , .5 , 0.522123f
+                for (char k = j; k != CharacterIterator.DONE; k = iterator.next()) {
+                    if (tokenString.length() > 0) {
+                        int j2 = tokenString.charAt(tokenString.length() - 1);
+                        if (j2 == 'f' || j2 == 'd') {
+                            // .2dddd or 0.223ffff ... multiple f/d not allowed or just break
+                            throw new LexerTokenException("Invalid decimal number");
+                        }
+                    }
+                    tokenString.append(k); // add the "." to the number
+                }
+                var token = new Token(tokenString.toString(), TokenType.Decimal);
+                tokens.add(token);
+                // invalid input: 2.  0.d
+            } else {
+                i = iterator.previous();
+                break;
+            }
+        }
+        var token = new Token(tokenString.toString(), TokenType.toNumber(i));
+        return token;
     }
 
     public List<Token> tokenize(File src) throws IOException {
