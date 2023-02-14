402b7f245dbc7ca0a6f02c1d8f167319265efe11
Tudor Pop <tudor14pop@gmail.com>
2023-02-15 01:43:28 +0200

    Add null literal

---
+++ src/main/java/dev/fangscl/ast/TypeSystem/Literals/NullLiteral.java
@@ -0,0 +1,18 @@
+package dev.fangscl.ast.TypeSystem.Literals;
+
+import dev.fangscl.ast.TypeSystem.Base.Expression;
+import dev.fangscl.ast.TypeSystem.NodeType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class NullLiteral extends Expression {
+    private String value;
+
+    public NullLiteral() {
+        this.kind = NodeType.NumericLiteral;
+        this.value = "null";
+    }
+
+}
