98ce8f84b8baa4a75b0c927e1d89f8869100ff33
Tudor Pop <tudor14pop@gmail.com>
2023-06-11 10:41:03 +0300

    Add some helper methods for Callable

--- src/main/java/dev/fangscl/Runtime/Functions/Numeric/MaxFunction.java
+++ src/main/java/dev/fangscl/Runtime/Functions/Numeric/MaxFunction.java
@@ -12,9 +12,9 @@ public class MaxFunction implements Callable {
     @Override
     public Object call(Interpreter interpreter, List<Object> args) {
         ArrayList<Comparable> arg = new ArrayList<>(args.size());
-        for (Object o : args) {
-            if (o instanceof Number){
-                arg.add((Comparable) o);
+        for (var it : args) {
+            if (it instanceof Number number){
+                arg.add((Comparable) number);
             } else {
                 throw new RuntimeException("invalid argument");
             }
--- src/test/java/dev/fangscl/Runtime/Functions/Numeric/MaxFunctionTest.java
+++ src/test/java/dev/fangscl/Runtime/Functions/Numeric/MaxFunctionTest.java
@@ -3,42 +3,34 @@ package dev.fangscl.Runtime.Functions.Numeric;
 import dev.fangscl.Runtime.BaseTest;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Log4j2
 class MaxFunctionTest extends BaseTest {
-    private MaxFunction function;
-
-    @BeforeEach
-    void init() {
-        function = new MaxFunction();
-    }
+    private final MaxFunction function = new MaxFunction();
 
     @Test
     void maxInt() {
-        var res = function.call(interpreter, List.of(1, 3, 2));
+        var res = function.call(interpreter, 1, 3, 2);
         assertEquals(3, res);
     }
 
     @Test
     void maxFloat() {
-        var res = function.call(interpreter, List.of(1.0f, 3.0f, 2.0f));
+        var res = function.call(interpreter, 1.0f, 3.0f, 2.0f);
         assertEquals(3.0f, res);
     }
 
     @Test
     void maxDouble() {
-        var res = function.call(interpreter, List.of(1.0d, 3.0d, 2.0d));
+        var res = function.call(interpreter, 1.0d, 3.0d, 2.0d);
         assertEquals(3.0d, res);
     }
 
     @Test
     void maxStr() {
-        Assertions.assertThrows(RuntimeException.class, () -> function.call(interpreter, List.of("1", "3", "2")));
+        Assertions.assertThrows(RuntimeException.class, () -> function.call(interpreter, "1", "3", "2"));
     }
 }
