97a2fd4217a844de5c28778c2f791928ecfed1ec
Tudor Pop <tudor14pop@gmail.com>
2024-05-26 18:11:39 +0300

    Replace hardcoding with src code

--- src/test/java/io/zmeu/Frontend/TypeChecker/BlockTest.java
+++ src/test/java/io/zmeu/Frontend/TypeChecker/BlockTest.java
@@ -3,35 +3,29 @@ package io.zmeu.Frontend.TypeChecker;
 import io.zmeu.types.Types;
 import org.junit.jupiter.api.Test;
 
-import static io.zmeu.Frontend.Parser.Expressions.BinaryExpression.binary;
-import static io.zmeu.Frontend.Parser.Expressions.VariableDeclaration.var;
-import static io.zmeu.Frontend.Parser.Literals.Identifier.id;
-import static io.zmeu.Frontend.Parser.Literals.NumberLiteral.number;
-import static io.zmeu.Frontend.Parser.Literals.StringLiteral.string;
-import static io.zmeu.Frontend.Parser.Statements.BlockExpression.block;
-import static io.zmeu.Frontend.Parser.Statements.ExpressionStatement.expressionStatement;
-import static io.zmeu.Frontend.Parser.Statements.VariableStatement.statement;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BlockTest extends BaseChecker {
 
     @Test
     void testBlock() {
-        var actual = checker.eval(block(
-                statement(var("x", number(10))),
-                statement(var("y", number(10))),
-                expressionStatement(binary("+", binary("*", "x", 2), id("y")))
-        ));
+        var actual = checker.eval(src("""
+                var x = 10
+                var y = 10
+                x*2+y
+                """));
         assertEquals(Types.Number, actual);
     }
 
     @Test
     void testNestedBlock() {
-        var actual = checker.eval(block(
-                statement(var("x", number(10))),
-                expressionStatement(block(statement(var("x", string("hello"))))),
-                expressionStatement(binary("*", "x", 2))
-        ));
+        var actual = checker.eval(src("""
+                var x = 10
+                {
+                    var x = "hello"
+                }
+                x * 2
+                """));
         assertEquals(Types.Number, actual);
     }
 
