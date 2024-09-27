46a1860b7ccf2dcb52cdca566e3d7ef6f9b213dc
Tudor Pop <tudor14pop@gmail.com>
2024-09-27 21:22:52 +0300

    Make visitor sealed

--- src/test/java/io/zmeu/TypeChecker/_SuiteTypeChecker.java
+++ src/test/java/io/zmeu/TypeChecker/_SuiteTypeChecker.java
@@ -3,6 +3,6 @@ package io.zmeu.TypeChecker;
 import org.junit.platform.suite.api.SelectPackages;
 
 @org.junit.platform.suite.api.Suite
-@SelectPackages({"io.zmeu.Frontend.TypeChecker"})
+@SelectPackages({"io.zmeu.TypeChecker"})
 public class _SuiteTypeChecker {
 }
