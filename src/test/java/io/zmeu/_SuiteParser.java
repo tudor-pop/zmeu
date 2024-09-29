1546dcf9ef05c7e6adf525ed04b15eda23f34d55
Tudor Pop <tudor14pop@gmail.com>
2024-09-29 15:09:25 +0300

    Allow void return type

--- src/test/java/io/zmeu/_SuiteParser.java
+++
@@ -1,8 +0,0 @@
-package io.zmeu;
-
-import org.junit.platform.suite.api.SelectPackages;
-
-@org.junit.platform.suite.api.Suite
-@SelectPackages({"io.zmeu.Frontend.Parse","io.zmeu.TypeChecker","io.zmeu.Frontend.Token","io.zmeu.Frontend.Lexical"})
-public class _SuiteParser {
-}
